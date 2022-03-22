47ee2d03f25229b8a1019854bd1c99eb5bed9a00
son1567 <101122156+son1567@users.noreply.github.com>
2022-03-23 05:07:58 +0700

    Add files via upload

---
+++ cờ Ba Lan.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle
+tl=turtle.Turtle()
+tl.speed(1)
+
+tl.forward(180)
+tl.right(90)
+tl.forward(120)
+tl.right(90)
+tl.forward(180)
+tl.right(90)
+tl.forward(120)
+tl.right(180)
+tl.forward(60)
+tl.fillcolor('red')
+tl.begin_fill()
+tl.left(90)
+tl.forward(180)
+tl.right(90)
+tl.forward(60)
+tl.right(90)
+tl.forward(180)
+tl.right(90)
+tl.forward(60)
+tl.end_fill()
+tl.begin_fill()
+turtle.done()
+
+
+# In[ ]:
+
+
+
+
---
+++ Đa giác đều.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle 
+
+tl= turtle.Turtle()
+tl.hideturtle()
+tl.speed(1)
+
+
+tl.fillcolor('blue')
+tl.begin_fill()
+tl.forward(80)
+tl.left(36)
+tl.forward(80)
+tl.left(36)
+tl.forward(80)
+tl.left(36)
+tl.forward(80)
+tl.left(36)
+tl.forward(80)
+tl.left(36)
+tl.forward(80)
+tl.left(36)
+tl.forward(80)
+tl.left(36)
+tl.forward(80)
+tl.left(36)
+tl.forward(80)
+tl.left(36)
+tl.forward(80)
+tl.end_fill()
+
+
+turtle.done()
+
